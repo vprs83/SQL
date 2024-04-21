0617ae49b491f05067913d3b8d861d66bd527e1a
Vladimirs Priklonskis <v.priklonskis@gmail.com>
2024-04-21 20:21:08 +0300

    update database

--- database.sql
+++ database.sql
@@ -130,10 +130,81 @@ DROP TABLE orders;
 DROP TABLE products;
 DROP TABLE shipper;
 DROP TABLE suppliers;
-        
-        
-        
-        
-        
-        
-        
+
+
+-------------------------- DB update --------------------------  
+-- create tables STORES, TARGETS
+CREATE TABLE stores(
+    StoreID     NUMBER          NOT NULL,
+    Address     VARCHAR2(20),
+    City        VARCHAR2(20),
+    PostalCode  VARCHAR2(20),
+    Country     VARCHAR2(20),
+    Phone       NUMBER,
+    
+    PRIMARY KEY(StoreID)
+);
+
+CREATE TABLE targets(
+    TargetID    NUMBER      NOT NULL,
+    StoreID     NUMBER      NOT NULL,
+    Year        VARCHAR2(4) NOT NULL,
+    Month       NUMBER      NOT NULL,
+    TargetSum   NUMBER,
+    
+    PRIMARY KEY(TargetID),
+    
+    FOREIGN KEY(StoreID)
+        REFERENCES stores(StoreID)
+);
+
+-- update table ORDERS
+ALTER TABLE orders ADD StoreID NUMBER; --NOT NULL;
+ALTER TABLE orders ADD FOREIGN KEY (StoreID) REFERENCES stores(StoreID);
+
+SELECT * FROM orders;        
+
+-- 1.
+INSERT INTO stores
+VALUES(1, 'First street 36', 'Los-Angeles', '15023', 'USA', 123456789);
+
+SELECT * FROM stores; 
+-- 2. now constraint NOT NULL can be added to attribute StoreID
+ALTER TABLE orders
+MODIFY StoreID NOT NULL;    -- ALTER COLUMN StoreID NOT NULL for SQL Server
+
+-- 3.
+UPDATE orders
+SET StoreID = 1
+WHERE StoreID IS NULL;
+
+INSERT INTO targets VALUES(1, 1, '1996', 7, 36000);
+INSERT INTO targets VALUES(2, 1, '1996', 8, 35000);
+INSERT INTO targets VALUES(3, 1, '1996', 9, 36000);
+INSERT INTO targets VALUES(4, 1, '1996', 10, 45000);
+INSERT INTO targets VALUES(5, 1, '1996', 11, 58000);
+INSERT INTO targets VALUES(6, 1, '1996', 12, 65000);
+INSERT INTO targets VALUES(7, 1, '1997', 1, 90000);
+INSERT INTO targets VALUES(8, 1, '1997', 2, 60000);
+INSERT INTO targets VALUES(9, 1, '1997', 3, 55000);
+INSERT INTO targets VALUES(10, 1, '1997', 4, 50000);
+INSERT INTO targets VALUES(11, 1, '1997', 5, 55000);
+INSERT INTO targets VALUES(12, 1, '1997', 6, 40000);
+INSERT INTO targets VALUES(13, 1, '1997', 7, 55000);
+INSERT INTO targets VALUES(14, 1, '1997', 8, 45000);
+INSERT INTO targets VALUES(15, 1, '1997', 9, 55000);
+INSERT INTO targets VALUES(16, 1, '1997', 10, 65000);
+INSERT INTO targets VALUES(17, 1, '1997', 11, 50000);
+INSERT INTO targets VALUES(18, 1, '1997', 12, 70000); 
+INSERT INTO targets VALUES(19, 1, '1998', 1, 12000);
+INSERT INTO targets VALUES(20, 1, '1998', 2, 12000);
+INSERT INTO targets VALUES(21, 1, '1998', 3, 10000);
+INSERT INTO targets VALUES(22, 1, '1998', 4, 11000);
+INSERT INTO targets VALUES(23, 1, '1998', 5, 15000);
+
+UPDATE targets SET TargetSum = 120000 WHERE TargetID = 19;
+UPDATE targets SET TargetSum = 120000 WHERE TargetID = 20;
+UPDATE targets SET TargetSum = 100000 WHERE TargetID = 21;
+UPDATE targets SET TargetSum = 110000 WHERE TargetID = 22;
+
+SELECT * FROM targets; 
