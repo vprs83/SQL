5d605c053c68af03c4a929bce5bbe8c606c1f6f9
Vladimirs Priklonskis <v.priklonskis@gmail.com>
2024-06-04 22:35:23 +0300

    add 3 solved tasks

--- stratascratch_practice.sql
+++ stratascratch_practice.sql
@@ -1050,3 +1050,60 @@ WITH median_writing_score_find AS (
 SELECT student_id
 FROM sat_scores
 WHERE sat_writing = (SELECT median FROM median_writing_score_find);
+
+/*  
+    ID 10061    Popularity of Hack  
+    Meta/Facebook has developed a new programing language called Hack.
+    To measure the popularity of Hack they ran a survey with their employees. 
+    The survey included data on previous programing familiarity as well as the number of years of experience, age, gender and most importantly satisfaction with Hack. 
+    Due to an error location data was not collected, but your supervisor demands a report showing average popularity of Hack by office location. 
+    Luckily the user IDs of employees completing the surveys were stored.
+    Based on the above, find the average popularity of the Hack per office location.
+    Output the location along with the average popularity.
+*/
+SELECT  DISTINCT e.location,
+        AVG(hs.popularity) OVER(PARTITION BY e.location)
+FROM facebook_employees e
+LEFT JOIN facebook_hack_survey hs ON e.id = hs.employee_id;
+
+/*
+    ID 9992     Find how many times each artist appeared on the Spotify ranking list
+    Find how many times each artist appeared on the Spotify ranking list.
+    Output the artist name along with the corresponding number of occurrences.
+    Order records by the number of occurrences in descending order.
+*/
+SELECT  DISTINCT artist,
+        COUNT(id) OVER(PARTITION BY artist) n_of_occurrences
+FROM spotify_worldwide_daily_song_ranking
+ORDER BY n_of_occurrences DESC;
+
+/*
+    ID 9881
+    Make a report showing the number of survivors and non-survivors by passenger class.
+    Classes are categorized based on the pclass value as:
+    pclass = 1: first_class
+    pclass = 2: second_classs
+    pclass = 3: third_class
+    Output the number of survivors and non-survivors by each class.
+*/
+SELECT  survived,
+        SUM(CASE 
+                WHEN pclass = 1 
+                THEN 1 
+                ELSE NULL
+            END
+            ) first_class,
+        SUM(CASE 
+                WHEN pclass = 2 
+                THEN 1 
+                ELSE NULL
+            END
+            ) second_class,
+        SUM(CASE 
+                WHEN pclass = 3 
+                THEN 1 
+                ELSE NULL 
+            END
+            ) third_class
+FROM titanic
+GROUP BY survived;
